1e8d78a01f328db6fed7597f11cae0727c100d22
Michael Wang <michaelw0395@gmail.com>
2019-08-22 12:04:46 +1000

    Completed Models, now starting with routes, controllers and views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Seeding the ingredients"
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "mango")
+Ingredient.create(name: "ginger")
+puts "Finished seeding ingredients!"
